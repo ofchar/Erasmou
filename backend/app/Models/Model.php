d3ac28a4a7b051f8887660e537d5b0c8e9712ea2
ofchar <matOwczarek@outlook.com>
2022-11-02 09:34:56 +0100

    Base Model class

---
+++ backend/app/Models/Model.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+
+class Model extends EloquentModel
+{
+    public function baseUuidScope(Builder $query, string $relationName, string $uuid) : Builder
+    {
+        return $query->whereHas($relationName, fn ($q) =>
+            $q->whereUuid($uuid)
+        );
+    }
+}
