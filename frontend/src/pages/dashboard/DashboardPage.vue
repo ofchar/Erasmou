e1016dcdbbcd33306b7767fa112f25cac8a7cd56
ofchar <matOwczarek@outlook.com>
2022-12-20 21:33:30 +0100

    simple dashboard page

--- frontend/src/pages/dashboard/DashboardPage.vue
+++ frontend/src/pages/dashboard/DashboardPage.vue
@@ -1,58 +1,135 @@
 <template>
-    <div class="hello">
-      <h1>{{ msg }}</h1>
-      <p>
-        For a guide and recipes on how to configure / customize this project,<br>
-        check out the
-        <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-      </p>
-      <h3>Installed CLI Plugins</h3>
-      <ul>
-        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      </ul>
-      <h3>Essential Links</h3>
-      <ul>
-        <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-        <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-        <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-        <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-        <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-      </ul>
-      <h3>Ecosystem</h3>
-      <ul>
-        <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-        <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-        <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-        <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-      </ul>
-    </div>
-  </template>
+	<div class="">
+		<header class="masthead">
+			<div class="container position-relative">
+				<div class="row justify-content-center">
+					<div class="col-xl-6">
+						<div class="text-center text-white">
+							<h1 class="mb-5">Make the most from your Erasmus with Erasmou!</h1>
 
-  <script>
-  export default {
-    name: 'HelloWorld',
-    props: {
-      msg: String
+							<div class="row justify-content-center" v-if="!isLogged">
+								<div class="col-md-3">
+									<router-link class='nav-link' to="/login"><button class="btn btn-primary btn-lg">I'm back!</button></router-link>
+
+								</div>
+								<div class="col-md-3">
+									<router-link class='nav-link' to="/register"><button class="btn btn-primary btn-lg">I'm new!</button></router-link>
+								</div>
+							</div>
+							<div class="row justify-content-center lead" v-else>
+								Welcome back {{ user.first_name }} {{ user.last_name }}!
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- Icons Grid-->
+		<section class="features-icons bg-light text-center">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-4">
+						<div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+							<div class="features-icons-icon d-flex justify-content-center">
+								<font-awesome-icon size="4x" icon="fa-solid fa-check-double"/>
+							</div>
+							<h3>100% verified</h3>
+							<p class="lead mb-0">We make sure that data comes only from verified Erasmus students.</p>
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+							<div class="features-icons-icon d-flex justify-content-center">
+								<font-awesome-icon size="4x" icon="fa-solid fa-sitemap" />
+							</div>
+							<h3>Organized for you</h3>
+							<p class="lead mb-0">All of the information neatly organized.</p>
+						</div>
+					</div>
+					<div class="col-lg-4">
+						<div class="features-icons-item mx-auto mb-0 mb-lg-3">
+							<div class="features-icons-icon d-flex justify-content-center">
+								icon here
+							</div>
+							<h3>Some other feature</h3>
+							<p class="lead mb-0">I'm yet to think of</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+</template>
+
+<script>
+export default {
+	components: {
+		//
+	},
+	props: [
+		//
+	],
+
+	data() {
+		return {
+			user: null,
+            isLogged: false,
+		}
+	},
+
+	watch: {
+		//
+	},
+	computed: {
+		//
+	},
+
+	methods: {
+		//
+	},
+
+	mounted() {
+		let user = localStorage.getItem('user');
+
+        if (user) {
+            this.user = JSON.parse(user);
+            this.isLogged = true;
+        }
+        else {
+            this.user = null;
+            this.isLogged = false;
+        }
+	},
+}
+</script>
+
+<style scoped lang="scss">
+.features-icons {
+    padding-top: 7rem;
+    padding-bottom: 7rem;
+
+    .features-icons-item {
+        max-width: 20rem;
+
+        .features-icons-icon {
+            height: 7rem;
+
+            i {
+                font-size: 4.5rem;
+            }
+        }
     }
-  }
-  </script>
+}
 
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
-  }
-  </style>
+header.masthead {
+    background-color: rgb(93, 93, 93);
+    // background: url('../assets/img/bg-masthead.jpg') no-repeat center center;
+    background-size: cover;
+    padding-top: 8rem;
+    padding-bottom: 8rem;
+
+    h1 {
+        font-size: 2rem;
+    }
+}
+</style>
