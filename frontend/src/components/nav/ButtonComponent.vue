b12b685f6c1926bd4c2e3bb8956840cb617c1812
ofchar <matOwczarek@outlook.com>
2023-05-16 22:33:36 +0200

    ButtonComponent v0.1

---
+++ frontend/src/components/nav/ButtonComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <button type="button" :class="setClass" @click="clicked">
+            <div v-if="loading" class="spinner-border spinner-border-sm"></div>
+            <div v-else><slot></slot></div>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+        //
+    },
+    props: {
+        setClass: {
+            default: "btn btn-primary"
+        },
+        loading: {
+            default: false,
+        },
+    },
+
+    data() {
+        return {
+            //
+        }
+    },
+
+    watch: {
+        //
+    },
+    computed: {
+        //
+    },
+
+    methods: {
+        clicked: function () {
+            this.$emit('clicked');
+        },
+    },
+
+    mounted() {
+        //
+    },
+}
+</script>
+
+<style></style>
